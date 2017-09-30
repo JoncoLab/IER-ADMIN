c0c9e741d27c930b781e0a81a9fc0d7dc5f2ca52
Saladin <nem97.sv@gmail.com>
2017-09-30 04:02:56 +0300

    firebase

---
+++ src/comp/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <nav>
+                <button>LOL</button>
+                <button>LOL</button>
+                <button>LOL</button>
+                <button>LOL</button>
+            </nav>
+        );
+    }
+}
